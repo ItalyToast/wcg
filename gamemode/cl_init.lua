6c7e4f1aa5359802c07f561bb249368943ec1228
ItalyToast <ItalyToast@gmail.com>
2017-04-12 19:30:10 +0200

    Removed dead code

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -34,11 +34,6 @@ end
 	Desc: Called every frame
 -----------------------------------------------------------]]
 function GM:Think()
-	local player = LocalPlayer()
-	if(IsValid(player) and player.GetXP != nil) then
-		local xp = player:GetXP()
-		XPBar.SetXP(XPBar, xp, 1000)
-	end
 end
 
 --[[---------------------------------------------------------
